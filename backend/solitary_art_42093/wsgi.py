c74aa9cb317781b5ed5cf38b6e9f1e018edf5b3b
Crowdbotics <team@crowdbotics.com>
2023-07-11 12:47:51 +0000

    Initial commit.

---
+++ backend/solitary_art_42093/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for solitary_art_42093 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solitary_art_42093.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-art-42093",
+  url: "https://solitary-art-42093.botics.co",
+  api: "https://solitary-art-42093.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
